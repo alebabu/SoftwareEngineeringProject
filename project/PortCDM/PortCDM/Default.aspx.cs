f644a64a92313710cf0fee9438cc77807ad3dcd5
Ello <ollepersson629@gmail.com>
2017-04-25 16:23:07 +0200

    Moved the backend code to a new directory and fixed a bug in the getPortCalls() method that caused the site to crash when getting portcalls twice

--- project/PortCDM/PortCDM/Default.aspx.cs
+++ project/PortCDM/PortCDM/Default.aspx.cs
@@ -8,13 +8,17 @@ namespace PortCDM
 {
 	public partial class Default : System.Web.UI.Page
 	{
-		protected void Page_Load(object sender, EventArgs e){
+        string id;
+		protected void Page_Load(object sender, EventArgs e)
+        {
 			messageConfirmationLiteral.Text = "";
+            
 		}
 
-		protected void sendMessage(object sender, EventArgs e){
-			PortCall pc = RestHandler.getPortCalls()[0];
-			messageConfirmationLiteral.Text = pc.id;
+		protected void sendMessage(object sender, EventArgs e)
+        {
+            id = RestHandler.getPortCalls()[0].id;
+            messageConfirmationLiteral.Text = id;
 		}
 	}
 }
