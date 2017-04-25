301699a4669e4b642618f9210f95e3319e5b3509
Ello <ollepersson629@gmail.com>
2017-04-25 16:28:03 +0200

    Forgot to actually commit the code

---
+++ project/PortCDM/PortCDM/Code/PortCall.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PortCDM_RestStructs
+{
+	public struct PortCall
+	{
+		public string id;
+		public Vessel vessel;
+		public string portUnLocode;
+		public string arrivalDate;
+		public string createdAt;
+		public string lastUpdate;
+		public string startTime;
+		public string endTime;
+	}
+}
