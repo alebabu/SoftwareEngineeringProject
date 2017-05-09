d08485fab010d317d70c3ef2921024fba99aa6e2
gusronmae <gusronmae@student.gu.se>
2017-05-09 14:54:33 +0200

    Hardcoded timeline

---
+++ project/PortCDM/PortCDM/Timeline.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PortCDM_RestStructs;
+using PortCDM_App_Code;
+
+namespace PortCDM
+{
+    public partial class Timeline : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<portCallMessage> list = RestHandler.getEvents();
+            eventListBox.DataSource = list;
+            eventListBox.DataBind();
+        }
+    }
+}
