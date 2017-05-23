1d9fa61ce86a6daea8bcfd44718e5db0b2c62f26
jtorbjornsson <jt@johannatorbjornsson.se>
2017-05-23 15:14:03 +0200

    should work on windows

--- project/PortCDM/PortCDM/Scripts/SelectShip.js
+++ project/PortCDM/PortCDM/Scripts/SelectShip.js
@@ -13,9 +13,17 @@
 }
 
 $(function() {
-	selectizeDropdown('#ctl00_cpMainContent_addShipDropDown');
 
-	var $select = $('#ctl00_cpMainContent_addShipDropDown').selectize();
+	if ($('#ctl00_cpMainContent_addShipDropDown')) {
+		var id = '#ctl00_cpMainContent_addShipDropDown'
+	} else {
+		var id = '#cpMainContent_addShipDropDown'
+	}
+	//selectizeDropdown('#ctl00_cpMainContent_addShipDropDown');
+	selectizeDropdown(id);
+
+	//var $select = $('#ctl00_cpMainContent_addShipDropDown').selectize();
+	var $select = $(id).selectize();
  	var control = $select[0].selectize;
  	control.clear();
 });
