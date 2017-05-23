8e5b3f2ec04b13ff2436c548b04f180ad161f6b1
gusbacke <gusbacke@student.gu.se>
2017-05-23 12:16:20 +0200

    Added UTC time on top of pages

---
+++ project/PortCDM/PortCDM/Scripts/CurrentTime.js
@@ -0,0 +1,17 @@
+﻿function padTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function currentTime() {
+    var utc = new Date();
+    var hours = padTime(utc.getUTCHours());
+    var minutes = padTime(utc.getUTCMinutes());
+    //var seconds = padTime(utc.getUTCSeconds());
+    document.getElementById('currentTime').innerHTML =
+        hours + ":" + minutes + " UTC";
+    t = setTimeout(function () { currentTime() }, 1000);
+}
+currentTime();
